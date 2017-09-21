0ef4b7ae80d2d7d85d3173b8889aa858f5b3bec5
ommaivnder <moseseribo@gmail.com>
2017-09-21 01:35:10 +0100

    update image paths
    issue with images showing up in webpage

--- scripts/getInfo.js
+++ scripts/getInfo.js
@@ -58,7 +58,7 @@ function getWeatherIcon(id){
   append a damn image...
   2.reason 1 is all i got
   **/
-    imageElement.setAttribute('src','./assets/weather-icons/all/_10d.svg');
+    imageElement.setAttribute('src','https://github.com/moseseribo/itsorng/tree/master/assets/weather-icons/all/_10d.svg');
     imageElement.setAttribute('id','weather-icon');
     iconDivElement.appendChild(imageElement);
 }
